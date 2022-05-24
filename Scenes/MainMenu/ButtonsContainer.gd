eb9e12c90d89d691e73db5900568a43f0aaac64b
3quarterscolon <94267030+3quarterscolon@users.noreply.github.com>
2022-05-24 16:48:42 -0400

    blah

--- Scenes/MainMenu/ButtonsContainer.gd
+++ Scenes/MainMenu/ButtonsContainer.gd
@@ -8,10 +8,10 @@ var size = self.rect_size / 2
 func _ready():
 	pass
 # HACK: Note to 3qc: If you use the "Full Rect" anchor in the editor, it will automatically center.
-## Called each frame.
-#func _process(delta):
-#	# FPS takes quite a hit on rising. TODO: Boost resize FPS
-#	newcenter = get_viewport_rect().size / 2
-#	if not newcenter == oldcenter:
-#		self.rect_position = Vector2(newcenter.x - size.x, newcenter.y - size.y)
-#	oldcenter = newcenter
+# Called each frame.
+func _process(delta):
+	# FPS takes quite a hit on rising. TODO: Boost resize FPS
+	newcenter = get_viewport_rect().size / 2
+	if not newcenter == oldcenter:
+		self.rect_position = Vector2(newcenter.x - size.x, newcenter.y - size.y)
+	oldcenter = newcenter
